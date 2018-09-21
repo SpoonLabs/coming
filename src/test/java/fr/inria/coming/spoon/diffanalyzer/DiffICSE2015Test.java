af14999f87c880f61f5820d22b5ce5f5411bafe1
martinezmatias <matias.sebastian.martinez@gmail.com>
2018-09-21 14:28:50 +0200

    [test] adding new test to check if path from Spoon works fine in the trees

--- src/test/java/fr/inria/coming/spoon/diffanalyzer/DiffICSE2015Test.java
+++ src/test/java/fr/inria/coming/spoon/diffanalyzer/DiffICSE2015Test.java
@@ -1,9 +1,17 @@
 package fr.inria.coming.spoon.diffanalyzer;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
 import org.junit.Test;
 
 import diffanalyzer.DiffICSE15ContextAnalyzer;
 import fr.inria.coming.main.ConfigurationProperties;
+import gumtree.spoon.AstComparator;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.path.CtPath;
 
 public class DiffICSE2015Test {
 
@@ -11,6 +19,21 @@ public class DiffICSE2015Test {
 	public void testICSE2015() throws Exception {
 		DiffICSE15ContextAnalyzer analyzer = new DiffICSE15ContextAnalyzer();
 		analyzer.run(ConfigurationProperties.getProperty("icse15difffolder"));
+	}
+
+	@Test
+	public void test_Path_from_Spoon() throws Exception {
+		AstComparator diff = new AstComparator();
+		File fl = new File("src/main/resources/diffcases/1139461/WildcardQuery/1139461_WildcardQuery_0_s.java");
+
+		CtType<?> astLeft = diff.getCtType(fl);
+
+		assertNotNull(astLeft);
+		assertEquals("WildcardQuery", astLeft.getSimpleName());
+
+		CtPath pathLeft = astLeft.getPath();
+		assertNotNull(pathLeft);
+
 	}
 
 }
