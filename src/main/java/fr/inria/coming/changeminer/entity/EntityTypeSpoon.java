5948c53c192065b9e03ca4ed010fb4b0e03ae599
martinezmatias <matias.sebastian.martinez@gmail.com>
2018-10-05 09:46:51 +0200

    Entity types enum

---
+++ src/main/java/fr/inria/coming/changeminer/entity/EntityTypeSpoon.java
@@ -0,0 +1,16 @@
+package fr.inria.coming.changeminer.entity;
+
+/**
+ * Entities from Spoon Model
+ * 
+ * @author Matias Martinez
+ *
+ */
+public enum EntityTypeSpoon {
+	ConstructorCall, Invocation, NewClass, Catch, Method, Lambda, Do, For, ForEach, While, Try, TryWithResource,
+	CatchVariableImpl, Annotation, Assignment, BinaryOperator, CodeSnippetExpression, Conditional, Assert, Literal,
+	OperatorAssignment, NewArray, TargetedExpression, UnaryOperator, TypeAccess, ArrayAccess, FieldAccess, SuperAccess,
+	ThisAccess, VariableRead, VariableWrite, FieldRead, FieldWrite, AnnotationFieldAccess, ArrayRead, ArrayWrite,
+	EnumValue, Block, Case, Field, CFlowBreak, TypeMember, LabelledFlowBreak, Return, Constructor, Interface, JavaDoag,
+	Type, Enum, Import, Comment, LocalVariable, Parameter, If, Synchronized, Throw
+}
