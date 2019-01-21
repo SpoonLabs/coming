81c61841971d817294e17a075d42426276f8bbb4
martinezmatias <matias.sebastian.martinez@gmail.com>
2019-01-21 13:42:29 +0100

    add null output

---
+++ src/main/java/fr/inria/coming/core/entities/output/NullOutput.java
@@ -0,0 +1,19 @@
+package fr.inria.coming.core.entities.output;
+
+import fr.inria.coming.changeminer.entity.FinalResult;
+import fr.inria.coming.core.entities.interfaces.IOutput;
+
+/**
+ * Null output
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class NullOutput implements IOutput {
+
+	@Override
+	public void generateOutput(FinalResult finalResult) {
+
+	}
+
+}
