27068846e49087d7cd88cf061adc576c72c7d24c
martinezmatias <matias.sebastian.martinez@gmail.com>
2019-03-17 11:09:10 -0300

    instance mining: now it saves a json file per revision

--- src/main/java/fr/inria/coming/core/entities/output/JSonPatternInstanceOutput.java
+++ src/main/java/fr/inria/coming/core/entities/output/JSonPatternInstanceOutput.java
@@ -17,7 +17,6 @@ import fr.inria.coming.changeminer.analyzer.instancedetector.PatternInstancesFro
 import fr.inria.coming.changeminer.analyzer.patternspecification.PatternAction;
 import fr.inria.coming.changeminer.entity.FinalResult;
 import fr.inria.coming.core.entities.RevisionResult;
-import fr.inria.coming.core.entities.interfaces.Commit;
 import fr.inria.coming.core.entities.interfaces.IOutput;
 import fr.inria.coming.main.ComingProperties;
 import gumtree.spoon.diff.operations.Operation;
@@ -27,61 +26,28 @@ public class JSonPatternInstanceOutput implements IOutput {
 	@Override
 	public void generateFinalOutput(FinalResult finalResult) {
 
-		System.out.println("JSON output");
+		System.out.println("JSON output Final Results");
 		JsonObject root = new JsonObject();
 		JsonArray instances = new JsonArray();
-		for (Object commit : finalResult.getAllResults().keySet()) {
+		root.add("instances", instances);
+		for (Object revision_commit : finalResult.getAllResults().keySet()) {
 
-			RevisionResult rv = (RevisionResult) finalResult.getAllResults().get(commit);
+			RevisionResult revisionResult = (RevisionResult) finalResult.getAllResults().get(revision_commit);
 
-			if (rv == null)
+			if (revisionResult == null)
 				continue;
 
-			PatternInstancesFromRevision result = (PatternInstancesFromRevision) rv
-					.getResultFromClass(PatternInstanceAnalyzer.class);
-
-			for (PatternInstancesFromDiff pi : result.getInfoPerDiff()) {
-				if (pi.getInstances().size() > 0) {
-					String cid = commit.toString();
-					if (commit instanceof Commit) {
-						Commit c = (Commit) commit;
-						cid = c.getName() + " " + c.getFullMessage();
-					}
-
-					System.out.println("\n--------\ncommit with instance:\n " + cid);
-					System.out.println(pi.getInstances());
-					for (ChangePatternInstance instancePattern : pi.getInstances()) {
-
-						JsonObject instance = new JsonObject();
-
-						instance.addProperty("revision", cid.toString());
-						instance.addProperty("pattern_name", (instancePattern.getPattern().getName()));
-						JsonArray ops = new JsonArray();
-
-						for (PatternAction pa : instancePattern.getActionOperation().keySet()) {
-							Operation op = instancePattern.getActionOperation().get(pa);
-							JsonObject opjson = new JsonObject();
-							opjson.addProperty("action", pa.getAction().toString());
-							opjson.addProperty("entity", pa.getAffectedEntity().toString());
-							opjson.add("op", getJSONFromOperator(op));
-							opjson.addProperty("code", op.getNode().toString());
-							opjson.addProperty("location", op.getNode().getPath().toString());
-							ops.add(opjson);
-						}
-
-						instance.add("ops", ops);
-						instances.add(instance);
-					}
-
-				}
-				root.add("instances", instances);
-			}
+			getInstancesOfRevision(revisionResult, instances);
 		}
 
+		saveToJsonFile(root, "all_instances_found");
+	}
+
+	public void saveToJsonFile(JsonObject root, String filename) {
 		File fout = new File(ComingProperties.getProperty("output"));
 		fout.mkdirs();
 		try {
-			FileWriter fw = new FileWriter(fout.getAbsolutePath() + File.separator + "instances_found" + ".json");
+			FileWriter fw = new FileWriter(fout.getAbsolutePath() + File.separator + filename + ".json");
 
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			JsonParser jp = new JsonParser();
@@ -94,6 +60,48 @@ public class JSonPatternInstanceOutput implements IOutput {
 			System.out.println("Output saved in " + fout.getAbsolutePath());
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	public void getInstancesOfRevision(RevisionResult revisionResult, JsonArray instances) {
+
+		String revisionIdentifier = null;
+		if (revisionResult.getRelatedRevision() != null) {
+			revisionIdentifier = revisionResult.getRelatedRevision().getName();
+		}
+
+		PatternInstancesFromRevision result = (PatternInstancesFromRevision) revisionResult
+				.getResultFromClass(PatternInstanceAnalyzer.class);
+
+		for (PatternInstancesFromDiff pi : result.getInfoPerDiff()) {
+			if (pi.getInstances().size() > 0) {
+
+				System.out.println("\n--------\ncommit with instance:\n " + revisionIdentifier);
+				System.out.println(pi.getInstances());
+				for (ChangePatternInstance instancePattern : pi.getInstances()) {
+
+					JsonObject instance = new JsonObject();
+
+					instance.addProperty("revision", revisionIdentifier.toString());
+					instance.addProperty("pattern_name", (instancePattern.getPattern().getName()));
+					JsonArray ops = new JsonArray();
+
+					for (PatternAction pa : instancePattern.getActionOperation().keySet()) {
+						Operation op = instancePattern.getActionOperation().get(pa);
+						JsonObject opjson = new JsonObject();
+						opjson.addProperty("action", pa.getAction().toString());
+						opjson.addProperty("entity", pa.getAffectedEntity().toString());
+						opjson.add("op", getJSONFromOperator(op));
+						opjson.addProperty("code", op.getNode().toString());
+						opjson.addProperty("location", op.getNode().getPath().toString());
+						ops.add(opjson);
+					}
+
+					instance.add("ops", ops);
+					instances.add(instance);
+				}
+
+			}
 		}
 	}
 
@@ -116,7 +124,13 @@ public class JSonPatternInstanceOutput implements IOutput {
 	}
 
 	@Override
-	public void generateRevisionOutput(RevisionResult resultAllAnalyzed) {
+	public void generateRevisionOutput(RevisionResult resultRevision) {
+		System.out.println("JSON output Final Results");
+		JsonObject root = new JsonObject();
+		JsonArray instances = new JsonArray();
+		root.add("instances", instances);
+		getInstancesOfRevision(resultRevision, instances);
+		saveToJsonFile(root, ("instances_found_rev_" + resultRevision.getRelatedRevision().getName()));
 
 	}
 }
