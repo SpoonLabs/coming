f761b6f9188b7d00b3b248b92a877122eb108089
Jian GU <ycaxgjd@gmail.com>
2019-03-24 17:01:28 +0100

    added support for ranking patches (#39)
    fixed bugs & improved implementation

--- src/main/java/fr/inria/prophet4j/Demo.java
+++ src/main/java/fr/inria/prophet4j/Demo.java
@@ -1,37 +1,59 @@
 package fr.inria.prophet4j;
 
-import fr.inria.prophet4j.defined.Structure.FeatureOption;
-import fr.inria.prophet4j.utility.dataport.Cardumen;
-//import fr.inria.prophet4j.utility.dataport.PGA;
-import fr.inria.prophet4j.utility.dataport.SANER;
+import fr.inria.prophet4j.dataset.DataManager;
+import fr.inria.prophet4j.defined.FeatureLearner;
+import fr.inria.prophet4j.defined.RepairEvaluator;
+import fr.inria.prophet4j.utility.Option;
+import fr.inria.prophet4j.utility.Option.DataOption;
+import fr.inria.prophet4j.utility.Option.PatchOption;
+
+import java.util.List;
 
 public class Demo {
+    private Option option;
+
+    public Demo(Option option) {
+        this.option = option;
+    }
+
+    private void learn() {
+        DataManager dataManager = new DataManager(option);
+        FeatureLearner featureLearner = new FeatureLearner(option);
+        List<String> filePaths = dataManager.func4Demo();
+        featureLearner.func4Demo(filePaths);
+    }
+
+    private void evaluate() {
+        RepairEvaluator repairEvaluator = new RepairEvaluator(option);
+        repairEvaluator.func4Demo();
+    }
+
     public static void main(String[] args) {
         try {
-//            new Cardumen().generateCSV(FeatureOption.ORIGINAL);
-            new Cardumen().handleData(FeatureOption.ORIGINAL);
-//            new PGA().handleCommits(FeatureOption.ORIGINAL);
-            new SANER().handleData(FeatureOption.ORIGINAL);
+            Option option = new Option();
+//            new Demo(option).learn();
+//            new Demo(option).evaluate();
 
-//            new Cardumen().handleData(FeatureOption.EXTENDED);
-//            new SANER().handleData(FeatureOption.EXTENDED);
+            option.patchOption = PatchOption.SPR;
+//            new Demo(option).learn();
+//            new Demo(option).evaluate();
+
+            option.dataOption = DataOption.SANER;
+//            new Demo(option).learn();
+//            new Demo(option).evaluate();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     /*
-    [original] Cardumen
-    19-03-19 12:09:37 INFO FeatureLearner:181 - 5-fold Cross Validation: 0.17570800656099123
-    [original] SANER
-    19-03-19 12:39:54 INFO FeatureLearner:181 - 5-fold Cross Validation: 0.011003139162889544
-    [extended] Cardumen
-    19-03-19 12:45:08 INFO FeatureLearner:181 - 5-fold Cross Validation: 0.16407816084612153
-    [extended] SANER
-    //
+    [original] Cardumen + Cardumen
+    19-03-24 12:59:17 INFO FeatureLearner:222 - 5-fold Cross Validation: 0.2716190501574554
+    [original] Cardumen + SPR
+    // i found NaN todo check
+    [original] SANER + SPR
+    19-03-24 12:48:47 INFO FeatureLearner:222 - 5-fold Cross Validation: 0.008142896242219158
      */
-    // todo 1. improve Feature (extended version)
-    // todo 2. improve FeatureCross (enhanced version)
-    // todo 3. improve learner algorithm (or implement other models?)
+    // todo (improve Feature or FeatureCross)
     // todo (try on other candidate-patches generators)
     // todo (draw graphs on 1k commits for Martin)
     // todo (integrate Coming and Prophet4J)
---
+++ src/main/java/fr/inria/prophet4j/dataset/DataManager.java
@@ -0,0 +1,216 @@
+package fr.inria.prophet4j.dataset;
+
+import fr.inria.prophet4j.defined.CodeDiffer;
+import fr.inria.prophet4j.defined.Structure.FeatureVector;
+import fr.inria.prophet4j.defined.Structure.Sample;
+import fr.inria.prophet4j.utility.Option;
+import fr.inria.prophet4j.utility.Option.DataOption;
+import fr.inria.prophet4j.utility.Option.PatchOption;
+import fr.inria.prophet4j.utility.Option.FeatureOption;
+import fr.inria.prophet4j.utility.Support;
+import fr.inria.prophet4j.utility.Support.DirType;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataManager {
+    private Option option;
+
+    public DataManager(Option option) {
+        this.option = option;
+    }
+
+    // avoid handling some cases with diff issues
+    // return List of VectorFilePath
+    private List<String> getBlacklist() {
+        List<String> blacklist = new ArrayList<>();
+        switch (option.patchOption) {
+            case CARDUMEN:
+                break;
+            case SPR:
+                switch (option.dataOption) {
+                    case CARDUMEN:
+                        break;
+                    case PGA:
+                        break;
+                    case SANER:
+                        blacklist.add("ignatov_intellij-erlang10/ErlangFormattingModelBuilder.java");
+                        blacklist.add("ignatov_intellij-erlang14/ErlangFormattingModelBuilder.java");
+                        blacklist.add("JetBrains_kotlin28/JetFormattingModelBuilder.java");
+                        blacklist.add("JetBrains_kotlin14/JetFormattingModelBuilder.java");
+                        break;
+                }
+                break;
+        }
+        return blacklist;
+    }
+
+    private Map<String, Map<File, File>> loadDataWithoutPatches(String dataPath) {
+        switch (option.dataOption) {
+            case CARDUMEN:
+                return DataLoader.loadCardumenDataWithoutPatches(dataPath, Support.PROPHET4J_DIR + "cardumen_dissection/");
+            case PGA:
+                break;
+            case SANER:
+                return DataLoader.loadSANERData(dataPath);
+        }
+        return new HashMap<>();
+    }
+
+    private Map<String, Map<File, List<File>>> loadDataWithPatches(String dataPath) {
+        switch (option.dataOption) {
+            case CARDUMEN:
+                return DataLoader.loadCardumenDataWithPatches(dataPath, Support.PROPHET4J_DIR + "cardumen_dissection/");
+            case PGA:
+                break;
+            case SANER:
+                break;
+        }
+        return new HashMap<>();
+    }
+
+    public List<String> func4Demo() {
+        if (option.dataOption == DataOption.CARDUMEN && option.patchOption == PatchOption.CARDUMEN) {
+            return handleDataWithoutGenerator();
+        } else {
+            return handleDataWithGenerator();
+        }
+    }
+
+    // buggy files & human patches are given
+    private List<String> handleDataWithGenerator() {
+        String dataPath = Support.getFilePath(DirType.DATA_DIR, option);
+        String featurePath = Support.getFilePath(DirType.FEATURE_DIR, option);
+        List<String> blackList = getBlacklist();
+
+        List<String> filePaths = new ArrayList<>();
+        String binFilePath = featurePath + "serial.bin";
+        if (new File(binFilePath).exists()) {
+            filePaths = DataHelper.deserialize(binFilePath);
+        } else {
+            CodeDiffer codeDiffer = new CodeDiffer(true, option);
+            Map<String, Map<File, File>> catalogs = loadDataWithoutPatches(dataPath);
+            int progressAll = catalogs.size(), progressNow = 0;
+            for (String pathName : catalogs.keySet()) {
+                Map<File, File> catalog = catalogs.get(pathName);
+                for (File oldFile : catalog.keySet()) {
+                    String vectorPath = pathName + "/" + oldFile.getName();
+                    System.out.println(vectorPath);
+                    if (blackList.contains(vectorPath)) {
+                        progressNow += 1;
+                        System.out.println("blacklist");
+                        continue;
+                    }
+                    vectorPath = featurePath + vectorPath;
+                    File vectorFile = new File(vectorPath);
+                    if (!vectorFile.exists()) {
+                        List<FeatureVector> featureVectors = codeDiffer.func4Demo(oldFile, catalog.get(oldFile));
+                        if (featureVectors.size() == 0) {
+                            // diff.commonAncestor() returns null value
+                            continue;
+                        }
+                        new Sample(vectorFile.getPath()).saveFeatureVectors(featureVectors);
+                    }
+                    filePaths.add(vectorPath);
+                }
+                progressNow += 1;
+                System.out.println(pathName + " : " + progressNow + " / " + progressAll);
+            }
+            DataHelper.serialize(binFilePath, filePaths);
+        }
+        return filePaths;
+    }
+
+    // buggy files & human patches & generated patches are given
+    private List<String> handleDataWithoutGenerator() {
+        String dataPath = Support.getFilePath(DirType.DATA_DIR, option);
+        String featurePath = Support.getFilePath(DirType.FEATURE_DIR, option);
+        List<String> blackList = getBlacklist();
+
+        List<String> filePaths = new ArrayList<>();
+        String binFilePath = featurePath + "serial.bin";
+        if (new File(binFilePath).exists()) {
+            filePaths = DataHelper.deserialize(binFilePath);
+        } else {
+            CodeDiffer codeDiffer = new CodeDiffer(true, option);
+            Map<String, Map<File, List<File>>> catalogs = loadDataWithPatches(dataPath);
+            int progressAll = catalogs.size(), progressNow = 0;
+            for (String pathName : catalogs.keySet()) {
+                Map<File, List<File>> catalog = catalogs.get(pathName);
+                for (File oldFile : catalog.keySet()) {
+                    try {
+                        String vectorPath = pathName + "/" + oldFile.getName();
+                        System.out.println(vectorPath);
+                        if (blackList.contains(vectorPath)) {
+                            progressNow += 1;
+                            System.out.println("blacklist");
+                            continue;
+                        }
+                        vectorPath = featurePath + vectorPath;
+                        File vectorFile = new File(vectorPath);
+                        if (!vectorFile.exists()) {
+                            List<FeatureVector> featureVectors = codeDiffer.func4Cardumen(oldFile, catalog.get(oldFile));
+                            if (featureVectors.size() == 0) {
+                                // diff.commonAncestor() returns null value
+                                continue;
+                            }
+                            new Sample(vectorFile.getPath()).saveFeatureVectors(featureVectors);
+                        }
+                        filePaths.add(vectorPath);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                progressNow += 1;
+                System.out.println(pathName + " : " + progressNow + " / " + progressAll);
+            }
+            DataHelper.serialize(binFilePath, filePaths);
+        }
+        return filePaths;
+    }
+
+    // by the way, feature types are reflected in prophet's way, which seems stricter
+    // generated patches: kth-tcs/overfitting-analysis(/dataport/Training/patched_cardumen/)
+    public void generateCSV(FeatureOption featureOption) throws NullPointerException {
+        String dataPath = Support.getFilePath(DirType.DATA_DIR, option);
+        String csvPath = Support.getFilePath(DirType.FEATURE_DIR, option);
+
+        // if only one large csv file is needed, we could just combine all generated ones
+        CodeDiffer codeDiffer = new CodeDiffer(false, option);
+        Map<String, Map<File, List<File>>> catalogs = loadDataWithPatches(dataPath);
+        for (String fileName : catalogs.keySet()) {
+            Map<String, List<FeatureVector>> metadata = new HashMap<>();
+            Map<File, List<File>> catalog = catalogs.get(fileName);
+            int progressAll = catalog.size(), progressNow = 0;
+            String csvFileName = csvPath + fileName + ".csv";
+            File csvFile = new File(csvFileName);
+            if (!csvFile.exists()) {
+                for (File oldFile : catalog.keySet()) {
+                    for (File newFile : catalog.get(oldFile)) {
+                        String buggyFileName = oldFile.getName(); // xxx.java
+                        String patchedFileName = newFile.getParentFile().getName(); // patchX
+                        String entryName = fileName + "-" + buggyFileName + "-" + patchedFileName;
+                        System.out.print(entryName);
+                        List<FeatureVector> featureVectors = codeDiffer.func4Demo(oldFile, newFile);
+                        if (featureVectors.size() == 0) {
+                            // diff.commonAncestor() returns null value
+                            System.out.println("patched file in patched_cardumen/ does not match patch file in cardumen/");
+                        }
+                        metadata.put(entryName, featureVectors);
+                        System.out.println("\tokay");
+                    }
+                    progressNow += 1;
+                    System.out.println(fileName + " : " + progressNow + " / " + progressAll);
+                }
+                // saveFeatureVectors as csv file
+                DataHelper.dumpCSV(csvFileName, metadata);
+                System.out.println("csv generated");
+            } else {
+                System.out.println("csv existed");
+            }
+        }
+    }
+}
