40f5a19d9b93e054ebe22f8523aef0e67728ce98
Siddharth Yadav <siddharth16268@iiitd.ac.in>
2019-06-19 13:27:51 +0200

    Added simple test case for JMutRepair

--- src/test/java/fr/inria/coming/spoon/repairability/RepairabilityTest.java
+++ src/test/java/fr/inria/coming/spoon/repairability/RepairabilityTest.java
@@ -1,20 +1,66 @@
 package fr.inria.coming.spoon.repairability;
 
+import fr.inria.coming.changeminer.analyzer.instancedetector.PatternInstancesFromRevision;
+import fr.inria.coming.changeminer.entity.FinalResult;
+import fr.inria.coming.changeminer.entity.IRevision;
+import fr.inria.coming.core.entities.RevisionResult;
 import fr.inria.coming.main.ComingMain;
+import fr.inria.coming.repairability.RepairabilityAnalyzer;
 import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class RepairabilityTest {
 
     @Test
-    public void testListEntities() {
+    public void testRepairabilityInterface() throws Exception {
 
-        ComingMain.main(new String[]{"-mode",
-                "repairability",
-                "-repairtool",
-                "JMutRepair",
-                "-input",
-                "files",
-                "-location",
-                getClass().getResource("/jMutRepairTest/").getFile()});
+        ComingMain cm = new ComingMain();
+
+        Object result = cm.run(
+                new String[]{"-mode",
+                        "repairability",
+                        "-repairtool",
+                        "JMutRepair",
+                        "-input",
+                        "files",
+                        "-location",
+                        getClass().getResource("/jMutRepairTest/").getFile()});
+
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testJMutRepairBasic() throws Exception {
+
+        ComingMain cm = new ComingMain();
+
+        FinalResult result = cm.run(
+                new String[]{"-mode",
+                        "repairability",
+                        "-repairtool",
+                        "JMutRepair",
+                        "-input",
+                        "files",
+                        "-location",
+                        getClass().getResource("/jMutRepairTest/").getFile()});
+
+        assertNotNull(result);
+
+        Map<IRevision, RevisionResult> revisionsMap = result.getAllResults();
+        assertEquals(4, revisionsMap.keySet().size());
+
+        int counter = 0;
+        for (Map.Entry<IRevision, RevisionResult> entry : revisionsMap.entrySet()) {
+            RevisionResult rr = entry.getValue();
+            PatternInstancesFromRevision instances = (PatternInstancesFromRevision) rr.getResultFromClass(RepairabilityAnalyzer.class);
+            counter += instances.getInfoPerDiff().stream().mapToInt(v -> v.getInstances().size()).sum();
+        }
+
+        assertEquals(3, counter);
+
     }
 }
