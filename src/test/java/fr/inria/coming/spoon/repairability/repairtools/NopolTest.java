34b7d7032e091f31c943e6f360a8af96b2e45c87
geekSiddharth <siddharth16268@iiitd.ac.in>
2019-06-26 12:17:24 +0200

    buggy checkpoint

--- src/test/java/fr/inria/coming/spoon/repairability/repairtools/NopolTest.java
+++ src/test/java/fr/inria/coming/spoon/repairability/repairtools/NopolTest.java
@@ -16,22 +16,21 @@ import static org.junit.Assert.assertNotNull;
 
 /**
  * Test files existence reasons
- *
+ * <p>
  * if-ins-shallow:
  * - patch1-Chart-13-Nopol2017
  * - patch1-Time-4-Nopol2017
  * - patch1-Closure-22-Nopol2017
- *
+ * <p>
  * if-ins-deep:
  * - patch1-Math-42-Nopol2017
  * - patch1-Closure-28-Nopol2017
- *
+ * <p>
  * if-update
  * - patch1-Closure-62-Nopol2017
  * - patch1-Math-81-Nopol2017
  * - patch1-Time-14-Nopol2017
  * - patch1-Closure-7-Nopol2017
- * 
  */
 public class NopolTest {
 
@@ -60,8 +59,10 @@ public class NopolTest {
             RevisionResult rr = entry.getValue();
             PatternInstancesFromRevision instances = (PatternInstancesFromRevision) rr.getResultFromClass(RepairabilityAnalyzer.class);
             int temp = instances.getInfoPerDiff().stream().mapToInt(v -> v.getInstances().size()).sum();
-            if(temp > 0){
+            if (temp > 0) {
                 counter += temp;
+            } else {
+                System.out.println(instances);
             }
         }
 
