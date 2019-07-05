a9d40c360a169f049e33c66b05e91196557ccc5f
Niloofar Tarighat <niloofartarighattrg@gmail.com>
2019-07-05 18:10:37 +0200

    added the JKali

--- src/test/java/fr/inria/coming/spoon/repairability/repairtools/JKaliTest.java
+++ src/test/java/fr/inria/coming/spoon/repairability/repairtools/JKaliTest.java
@@ -5,10 +5,10 @@ import fr.inria.coming.changeminer.entity.FinalResult;
 import fr.inria.coming.spoon.repairability.TestUtills;
 import org.junit.Test;
 
-public class JKali {
+public class JKaliTest {
     @Test
-    public void testJMutRepairTruePositive1() throws Exception {
-        FinalResult result = TestUtills.runRepairability("JKali", "/repairability_test_files/JKali/");
+    public void testJKali1() throws Exception {
+        FinalResult result = TestUtills.runRepairability("JKaliTest", "/repairability_test_files/JKali/");
         TestUtills.numberOfInstances(result, 5, 5);
     }
 
