9e835f7041ec3c787550fdef422217dfa34937f5
Jian GU <jaxxgu@gmail.com>
2019-07-26 13:24:22 +0200

    try `codesigning.asc.enc` again

--- .buildscript/deploy_to_maven.sh
+++ .buildscript/deploy_to_maven.sh
@@ -23,10 +23,10 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
  else
   echo "Deploying ..."
 
-  # made with "travis encrypt-file signingkey.asc -r SpoonLabs/coming --add"
-  openssl aes-256-cbc -K $encrypted_a263e63e6aa6_key -iv $encrypted_a263e63e6aa6_iv -in .buildscript/signingkey.asc.enc -out signingkey.asc -d
+  # made with "travis encrypt-file codesigning.asc -r SpoonLabs/coming --add"
+  openssl aes-256-cbc -K $encrypted_a263e63e6aa6_key -iv $encrypted_a263e63e6aa6_iv -in .buildscript/codesigning.asc.enc -out codesigning.asc -d
   echo "Before gpg"
-  gpg2 --fast-import signingkey.asc
+  gpg2 --fast-import codesigning.asc
   echo "After gpg"
 
   # getting the previous version on Maven Central
@@ -36,6 +36,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   # and incrementing it
   mvn versions:set -DnewVersion=1.$((PREVIOUS_MAVEN_CENTRAL_VERSION+1))
   echo "Starting deployment using maven deploy ..."
-  mvn -Prelease deploy --settings .buildscript/settings.xml -Dmaven.test.skip=true #-Dgpg.passphrase=$PASSPHRASE
+  mvn -Prelease deploy --settings .buildscript/settings.xml -Dmaven.test.skip=true -Dgpg.passphrase=$PASSPHRASE
   echo "Well deployed!"
 fi
