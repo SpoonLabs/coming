0067474ed6579fef7ce2c1a17a72a437e4caa288
Niloofar Tarighat <niloofartarighattrg@gmail.com>
2019-07-30 13:23:39 +0200

    modify log's

--- src/main/java/fr/inria/coming/changeminer/analyzer/instancedetector/PatternInstancesFromDiff.java
+++ src/main/java/fr/inria/coming/changeminer/analyzer/instancedetector/PatternInstancesFromDiff.java
@@ -2,9 +2,11 @@ package fr.inria.coming.changeminer.analyzer.instancedetector;
 
 import java.util.List;
 
+import fr.inria.coming.changeminer.analyzer.commitAnalyzer.FineGrainDifftAnalyzer;
 import fr.inria.coming.changeminer.entity.IRevision;
 import fr.inria.coming.core.entities.AnalysisResult;
 import gumtree.spoon.diff.Diff;
+import org.apache.log4j.Logger;
 
 /**
  * 
@@ -16,6 +18,8 @@ public class PatternInstancesFromDiff extends AnalysisResult<IRevision> {
 	protected List<ChangePatternInstance> instances = null;
 	protected Diff diff = null;
 	protected String location = null;
+
+    Logger log = Logger.getLogger(this.getClass().getName());
 
 	public PatternInstancesFromDiff(IRevision analyzed) {
 		super(analyzed);
@@ -35,11 +39,13 @@ public class PatternInstancesFromDiff extends AnalysisResult<IRevision> {
 		this.location = location;
 	}
 
+
 	@Override
 	public String toString() {
 		try {
 			if (diff == null) {
-				System.err.println("Diff null");
+			    log.error("Diff null");
+//				System.err.println("Diff null");
 				return "--Diff null--";
 			}
 			String diffString = "";
@@ -48,7 +54,8 @@ public class PatternInstancesFromDiff extends AnalysisResult<IRevision> {
 				diffString = diff.toString();
 
 			} catch (Exception e) {
-				System.err.println("Error when printing diff result: " + e.getMessage());
+                log.error("Error when printing diff result: " + e.getMessage());
+//				System.err.println("Error when printing diff result: " + e.getMessage());
 
 				e.printStackTrace();
 				diffString = "wrong diff";
@@ -56,11 +63,14 @@ public class PatternInstancesFromDiff extends AnalysisResult<IRevision> {
 			String resultString = "\n----For Diff:" + diffString + "\n: number instances found: " + instances.size();
 			for (ChangePatternInstance instance : instances) {
 				resultString += "\n" + instance.toString();
+
 			}
-			return resultString += "\n----";
+            log.info(resultString += "\n----");
+//			return resultString += "\n----";
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		log.info("--Diff ex--");
 		return "--Diff ex--";
 	}
 
