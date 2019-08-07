c1c5db6c1041b9862b5ce11999b472189cb361e7
Jian GU <ycaxgjd@gmail.com>
2019-08-07 14:16:45 +0200

    appended one API class, uploaded some pre-trained ParameterVector files (#147)
    * one more try
    
    * try `codesigning.asc.enc` again
    
    * appended one API class, uploaded some pre-trained ParameterVector files.

---
+++ src/main/java/fr/inria/prophet4j/P4J.java
@@ -0,0 +1,37 @@
+package fr.inria.prophet4j;
+
+import fr.inria.prophet4j.utility.CodeDiffer;
+import fr.inria.prophet4j.utility.Option;
+import fr.inria.prophet4j.utility.Option.DataOption;
+import fr.inria.prophet4j.utility.Option.FeatureOption;
+import fr.inria.prophet4j.utility.Option.PatchOption;
+import fr.inria.prophet4j.utility.Option.LearnerOption;
+import fr.inria.prophet4j.utility.Structure;
+
+import java.io.File;
+import java.util.List;
+
+// this is the API class of Prophet4J
+public class P4J {
+    private CodeDiffer codeDiffer;
+    private Structure.ParameterVector parameterVector;
+
+    public P4J() {
+        Option option = new Option();
+        option.dataOption = DataOption.BUG_DOT_JAR_MINUS_MATH;
+        option.patchOption = PatchOption.BUG_DOT_JAR_MINUS_MATH;
+        option.featureOption = FeatureOption.ORIGINAL;
+        option.learnerOption = LearnerOption.CROSS_ENTROPY;
+
+        this.codeDiffer = new CodeDiffer(false, option);
+        this.parameterVector = new Structure.ParameterVector(option.featureOption);
+    }
+
+    public double computeOverfittingScore(File buggyFile, File patchedFile) {
+        List<Structure.FeatureMatrix> featureMatrices = codeDiffer.runByGenerator(buggyFile, patchedFile);
+        if (featureMatrices.size() == 1) {
+            return featureMatrices.get(0).score(parameterVector);
+        }
+        return 0;
+    }
+}
