b68eb16e1de059ffefed2f2097caf31c21aad32a
niloofartrg <niloofartarighattrg@gmail.com>
2019-09-13 10:36:33 +0200

    Update RepairabilityTest.java

--- src/test/java/fr/inria/coming/spoon/repairability/RepairabilityTest.java
+++ src/test/java/fr/inria/coming/spoon/repairability/RepairabilityTest.java
@@ -19,7 +19,8 @@ import java.util.Set;
 import static org.junit.Assert.*;
 
 public class RepairabilityTest {
-
+   
+   @Test
    public void testRepairabilityInterface() throws Exception {
         TestUtills.runRepairability("JMutRepair", "/repairability_test_files/one/");
 
