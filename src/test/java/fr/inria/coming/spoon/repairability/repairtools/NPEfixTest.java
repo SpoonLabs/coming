a8b979e1524e9dd04279c576902c8820b84006bc
khashayar etemadi <khaes@kth.se>
2020-01-30 19:08:24 +0100

    npefix tested on ground-truth and some bugs fixed

--- src/test/java/fr/inria/coming/spoon/repairability/repairtools/NPEfixTest.java
+++ src/test/java/fr/inria/coming/spoon/repairability/repairtools/NPEfixTest.java
@@ -2,16 +2,41 @@ package fr.inria.coming.spoon.repairability.repairtools;
 
 
 import fr.inria.coming.changeminer.entity.FinalResult;
+import fr.inria.coming.main.ComingMain;
 import fr.inria.coming.spoon.repairability.RepairabilityTestUtils;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
 import org.junit.Test;
 
 public class NPEfixTest {
+	
+	@Test
+	public void testGroundTruthCreatedPatches() throws Exception {
+    	RepairabilityTestUtils.checkGroundTruthPatches(getClass(), "NPEfix", 0, 0);
+	}
 
-    @Test
-    public void NPEfixTest() throws Exception {
-        FinalResult result = RepairabilityTestUtils.runRepairability("NPEfix", "/repairability_test_files/NPEfix/");
-        RepairabilityTestUtils.checkNumberOfRepairInstances(result, 13, 11);
+	@Test
+	public void testFilePairs() throws Exception {
 
-        //18 is FN
-    }
+		File left = getFile("repairability_test_files/ground_truth/NPEfix/patch7-Math-1115/NPEfix/00125/NPEfix_00125_s.java");
+		File right = getFile("repairability_test_files/ground_truth/NPEfix/patch7-Math-1115/NPEfix/00125/NPEfix_00125_t.java");
+
+		ComingMain cm = new ComingMain();
+
+		FinalResult result = cm
+				.run(new String[] { "-location", left.getAbsolutePath() + File.pathSeparator + right.getAbsolutePath(),
+						"-input", "filespair", "-mode", "repairability", "-repairtool", "NPEfix", "-parameters", 
+						"include_all_instances_for_each_tool:true:max_nb_commit_analyze:300:max_time_for_a_git_repo:-1"});
+
+		assertNotNull(result);
+	}
+	
+	public File getFile(String name) {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(name).getFile());
+		return file;
+	}
 }
