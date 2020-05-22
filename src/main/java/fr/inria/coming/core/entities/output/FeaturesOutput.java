6c5a4ad20a672aabf502ef4b7ed75eeb392ec374
Sophie H Ye <he_ye_90s@hotmail.com>
2020-05-22 19:12:38 +0200

    Update FeaturesOutput.java

--- src/main/java/fr/inria/coming/core/entities/output/FeaturesOutput.java
+++ src/main/java/fr/inria/coming/core/entities/output/FeaturesOutput.java
@@ -2,7 +2,11 @@ package fr.inria.coming.core.entities.output;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
@@ -69,6 +73,7 @@ public class FeaturesOutput implements IOutput {
 			Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 			JsonParser jp = new JsonParser();
 			JsonElement je = jp.parse(file.toString());
+			sort(je);
 			String prettyJsonString = gson.toJson(je);
 			log.debug("\nJSON Code Change feature: (file stored at " + fileName + ")\n");
 			fw.write(prettyJsonString);
@@ -86,5 +91,56 @@ public class FeaturesOutput implements IOutput {
 		save(comingFeatures);
 		
 	}
+	
+	   private static Comparator<String> getComparator()
+	    {
+	        Comparator<String> c = new Comparator<String>()
+	        {
+	            public int compare(String o1, String o2)
+	            {
+	                return o1.compareTo(o2);
+	            }
+	        };
 
+	        return c;
+	    }
+	
+	public static void sort(JsonElement e) {
+       if (e.isJsonNull())
+       {
+           return;
+       }
+
+       if (e.isJsonPrimitive())
+       {
+           return;
+       }
+
+       if (e.isJsonArray())
+       {
+           JsonArray a = e.getAsJsonArray();
+           for (Iterator<JsonElement> it = a.iterator(); it.hasNext();)
+           {
+               sort(it.next());
+           }
+           return;
+       }
+
+       if (e.isJsonObject())
+       {
+           Map<String, JsonElement> tm = new TreeMap<String, JsonElement>(getComparator());
+           for (Entry<String, JsonElement> en : e.getAsJsonObject().entrySet())
+           {
+               tm.put(en.getKey(), en.getValue());
+           }
+
+           for (Entry<String, JsonElement> en : tm.entrySet())
+           {
+               e.getAsJsonObject().remove(en.getKey());
+               e.getAsJsonObject().add(en.getKey(), en.getValue());
+               sort(en.getValue());
+           }
+           return;
+       }
+   }
 }
