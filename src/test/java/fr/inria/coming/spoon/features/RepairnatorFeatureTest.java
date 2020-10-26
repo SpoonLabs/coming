127afd0d1c0626d443d051ce8f6fbab8919a20d3
Sophie H Ye <he_ye_90s@hotmail.com>
2020-10-26 14:31:30 +0100

    updated ODS label from string value to enum (#239)

--- src/test/java/fr/inria/coming/spoon/features/RepairnatorFeatureTest.java
+++ src/test/java/fr/inria/coming/spoon/features/RepairnatorFeatureTest.java
@@ -4,19 +4,20 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import org.junit.Test;
 import fr.inria.coming.codefeatures.RepairnatorFeatures;
+import fr.inria.coming.codefeatures.RepairnatorFeatures.ODSLabel;
 
 public class RepairnatorFeatureTest {
  
 	 	@Test
 		public void correctPatch() throws Exception  {
 		 File pairFolder = new File("src/main/resources/Defects4J_all_pairs/Lang_36");
-		 String label = new RepairnatorFeatures().getLabel(pairFolder);
-		 assertEquals(label,"correct");
+		 ODSLabel label = new RepairnatorFeatures().getLabel(pairFolder);
+		 assertEquals(label,ODSLabel.CORRECT);
 	 }
 	 	@Test
 	 	public void unknownPatch() throws Exception  {
 			 File pairFolder = new File("src/main/resources/Defects4J_all_pairs/Math_42");
-			 String label = new RepairnatorFeatures().getLabel(pairFolder);
-			 assertEquals(label,"unknown");
+			 ODSLabel label = new RepairnatorFeatures().getLabel(pairFolder);
+			 assertEquals(label,ODSLabel.UNKNOWN);
 		 }
 }
