09992e7b8b906dce95f73812161e8e9205791378
martinezmatias <matias.sebastian.martinez@gmail.com>
2020-11-18 09:19:18 +0100

    get the ods model from resource as inputstream

--- src/main/java/fr/inria/coming/codefeatures/RepairnatorFeatures.java
+++ src/main/java/fr/inria/coming/codefeatures/RepairnatorFeatures.java
@@ -1,12 +1,16 @@
 package fr.inria.coming.codefeatures;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import add.main.Config;
+
+import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -14,8 +18,7 @@ import com.google.gson.JsonObject;
 
 import add.entities.RepairPatterns;
 import add.features.detector.repairpatterns.RepairPatternDetector;
-
-import org.apache.log4j.Logger;
+import add.main.Config;
 import fr.inria.coming.main.ComingProperties;
 import fr.inria.prophet4j.utility.CodeDiffer;
 import fr.inria.prophet4j.utility.Option;
@@ -23,161 +26,182 @@ import fr.inria.prophet4j.utility.Option.FeatureOption;
 import fr.inria.prophet4j.utility.Structure.FeatureMatrix;
 import gumtree.spoon.AstComparator;
 import gumtree.spoon.diff.Diff;
-
-import ml.dmlc.xgboost4j.java.DMatrix;
 import ml.dmlc.xgboost4j.java.Booster;
+import ml.dmlc.xgboost4j.java.DMatrix;
 import ml.dmlc.xgboost4j.java.XGBoost;
 
 public class RepairnatorFeatures {
 
+	public static final String ODS_MODEL = "ODSmodel";
 	protected static Logger log = Logger.getLogger(Thread.currentThread().getName());
-    
+
 	public enum ODSLabel {
-		UNKNOWN,
-		CORRECT,
-		OVERFITTING;
+		UNKNOWN, CORRECT, OVERFITTING;
 	}
 
-	
 	public ODSLabel getLabel(File pairFolder) throws Exception {
-		
-		//Obtain feature in libsvm format
+
+		// Obtain feature in libsvm format
 		String features = extractFeatures(pairFolder);
-		if("".equals(features)) {
+		if ("".equals(features)) {
 			return ODSLabel.UNKNOWN;
 		}
-		//Load test data in libsvm format
+		// Load test data in libsvm format
 		File tempFile = File.createTempFile("test", ".txt");
 		try (FileWriter sb = new FileWriter(tempFile)) {
 			sb.append(features);
 		}
-		//Load model
+		// Load model
 		DMatrix dtest = new DMatrix(tempFile.getAbsolutePath());
-		String odsModel = ComingProperties.getProperty("ODSmodel");  
-		Booster booster = XGBoost.loadModel(odsModel);
-		
-		//Predict
+
+		InputStream inODSModelPath = retrieveStreamOfODSModel();
+
+		Booster booster = XGBoost.loadModel(inODSModelPath);
+
+		// Predict
 		float[][] predicts = booster.predict(dtest);
 		float probability = predicts[0][0];
 		tempFile.delete();
 
-		
-		//Return label		
+		// Return label
 		String thresholdString = ComingProperties.getProperty("overfitting-threshold");
 		float threshold = Float.parseFloat(thresholdString);
 
 		if (probability > threshold) {
 			return ODSLabel.OVERFITTING;
-		} else {		
+		} else {
 			return ODSLabel.CORRECT;
 		}
-		
+
 	}
-	
-	
+
+	/**
+	 * Get the input stream of the model according to the path of the ODS model.
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public InputStream retrieveStreamOfODSModel() throws Exception {
+
+		String odsModelPath = ComingProperties.getProperty(ODS_MODEL);
+		File modelFile = new File(odsModelPath);
+		InputStream in = null;
+		if (modelFile.exists()) {
+			in = new FileInputStream(modelFile);
+		} else {
+			in = getClass().getResourceAsStream(odsModelPath);
+		}
+		if (in != null)
+			return in;
+		else {
+			String message = "Problems to load the ODS model: " + odsModelPath;
+			log.error(message);
+			throw new FileNotFoundException(message);
+		}
+
+	}
+
 	public void trainModel(File pairFolder) throws Exception {
-		
-		String odsModel = ComingProperties.getProperty("ODSmodel");
-		String trainingFile = ComingProperties.getProperty("ODSTrainingFile");  
-		DMatrix trainMat = new DMatrix(trainingFile);		
+
+		String odsModel = ComingProperties.getProperty(ODS_MODEL);
+		String trainingFile = ComingProperties.getProperty("ODSTrainingFile");
+		DMatrix trainMat = new DMatrix(trainingFile);
 		Map<String, Object> params = new HashMap<String, Object>() {
-			  {
-			    put("eta", ComingProperties.getProperty("xgboost-training-eta"));
-			    put("max_depth", ComingProperties.getProperty("xgboost-training-max-depth"));
-			    put("gamma", ComingProperties.getProperty("xgboost-training-gamma"));
-			    put("objective", ComingProperties.getProperty("xgboost-training-objective"));
-			    put("eval_metric",  ComingProperties.getProperty("xgboost-training-eval-metric"));
-			  }
-			};
-			
+			{
+				put("eta", ComingProperties.getProperty("xgboost-training-eta"));
+				put("max_depth", ComingProperties.getProperty("xgboost-training-max-depth"));
+				put("gamma", ComingProperties.getProperty("xgboost-training-gamma"));
+				put("objective", ComingProperties.getProperty("xgboost-training-objective"));
+				put("eval_metric", ComingProperties.getProperty("xgboost-training-eval-metric"));
+			}
+		};
+
 		Map<String, DMatrix> watches = new HashMap<String, DMatrix>() {
-				{
-				    put("train", trainMat);
-		      }
-		};		
-							
+			{
+				put("train", trainMat);
+			}
+		};
+
 		int nround = Integer.parseInt(ComingProperties.getProperty("xgboost-training-nround"));
-		Booster booster = XGBoost.train(trainMat, params, nround, watches, null, null);			
+		Booster booster = XGBoost.train(trainMat, params, nround, watches, null, null);
 		booster.saveModel(odsModel);
 
 	}
-	
-	
+
 	public String extractFeatures(File pairFolder) throws Exception {
-		
+
 		try {
-		String features = "";
-		
-		// Get the src and target file paths
-		Map<String, File> srcTargetPair = getSrcTargetPair(pairFolder);
-		File previousVersion = srcTargetPair.get("src");
-		File postVersion = srcTargetPair.get("target");
+			String features = "";
 
+			// Get the src and target file paths
+			Map<String, File> srcTargetPair = getSrcTargetPair(pairFolder);
+			File previousVersion = srcTargetPair.get("src");
+			File postVersion = srcTargetPair.get("target");
 
-		// Compute the diff with Gumtree
-		AstComparator comparator = new AstComparator();
-		Diff diff = comparator.compare(previousVersion, postVersion);
+			// Compute the diff with Gumtree
+			AstComparator comparator = new AstComparator();
+			Diff diff = comparator.compare(previousVersion, postVersion);
 
-		// Case: No diff detected, we return empty feature string
-		if (diff.getRootOperations().size() == 0) {
-			System.out.print("diff is null: " + previousVersion.getAbsolutePath());
-			return features;
-		}
+			// Case: No diff detected, we return empty feature string
+			if (diff.getRootOperations().size() == 0) {
+				System.out.print("diff is null: " + previousVersion.getAbsolutePath());
+				return features;
+			}
 
-		// Extract code description features (P4J)
-		Option option = new Option();
-		option.featureOption = FeatureOption.ORIGINAL;
-		Boolean cross = ComingProperties.getPropertyBoolean("cross");
-		String codefeature = ComingProperties.getProperty("codeDescriptionFeatures");
-		String[] codefeatureList = codefeature.split(",");
-		CodeDiffer codeDiffer = new CodeDiffer(false, option, cross);
+			// Extract code description features (P4J)
+			Option option = new Option();
+			option.featureOption = FeatureOption.ORIGINAL;
+			Boolean cross = ComingProperties.getPropertyBoolean("cross");
+			String codefeature = ComingProperties.getProperty("codeDescriptionFeatures");
+			String[] codefeatureList = codefeature.split(",");
+			CodeDiffer codeDiffer = new CodeDiffer(false, option, cross);
 
-		List<FeatureMatrix> featureMatrix = codeDiffer.runByGenerator(previousVersion, postVersion);
-		JsonObject jsonfile = new P4JFeatureAnalyzer().getSimleP4JJSON(option, postVersion, featureMatrix, true);
+			List<FeatureMatrix> featureMatrix = codeDiffer.runByGenerator(previousVersion, postVersion);
+			JsonObject jsonfile = new P4JFeatureAnalyzer().getSimleP4JJSON(option, postVersion, featureMatrix, true);
 
-		for (String feature : codefeatureList) {
-			for (Entry<String, JsonElement> extractedFeatureSet : jsonfile.entrySet()) {
-				if (feature.contains(extractedFeatureSet.getKey())) {
-					features += extractedFeatureSet.getValue() + ",";
+			for (String feature : codefeatureList) {
+				for (Entry<String, JsonElement> extractedFeatureSet : jsonfile.entrySet()) {
+					if (feature.contains(extractedFeatureSet.getKey())) {
+						features += extractedFeatureSet.getValue() + ",";
+					}
 				}
 			}
-		}
-		features = features.replace("\"", "");
+			features = features.replace("\"", "");
 
-		// Extract repair pattern features (ADD)
-		String repairFeature = ComingProperties.getProperty("repairPatternFeatures");
-		String[] repairFeatureList = repairFeature.split(",");
-		File tempFile = File.createTempFile("add_", ".diff");
-		Config config = new Config();
-		config.setDiffPath(tempFile.getAbsolutePath());
-		config.setBuggySourceDirectoryPath(pairFolder.getAbsolutePath());
-		RepairPatternDetector patternDetector = new RepairPatternDetector(config, diff);
-		RepairPatterns analyze = patternDetector.analyze();
+			// Extract repair pattern features (ADD)
+			String repairFeature = ComingProperties.getProperty("repairPatternFeatures");
+			String[] repairFeatureList = repairFeature.split(",");
+			File tempFile = File.createTempFile("add_", ".diff");
+			Config config = new Config();
+			config.setDiffPath(tempFile.getAbsolutePath());
+			config.setBuggySourceDirectoryPath(pairFolder.getAbsolutePath());
+			RepairPatternDetector patternDetector = new RepairPatternDetector(config, diff);
+			RepairPatterns analyze = patternDetector.analyze();
 
-		JsonObject repairfeatures = new Gson().fromJson(analyze.toJson().toString(), JsonObject.class);
-		JsonObject repairPatterns = (JsonObject) repairfeatures.get("repairPatterns");
+			JsonObject repairfeatures = new Gson().fromJson(analyze.toJson().toString(), JsonObject.class);
+			JsonObject repairPatterns = (JsonObject) repairfeatures.get("repairPatterns");
 
-		for (String feature : repairFeatureList) {
-			for (Entry<String, JsonElement> extractedFeatureSet : repairPatterns.entrySet()) {
-				if (feature.contains(extractedFeatureSet.getKey())) {
-					features += extractedFeatureSet.getValue() + ",";
+			for (String feature : repairFeatureList) {
+				for (Entry<String, JsonElement> extractedFeatureSet : repairPatterns.entrySet()) {
+					if (feature.contains(extractedFeatureSet.getKey())) {
+						features += extractedFeatureSet.getValue() + ",";
+					}
 				}
 			}
-		}
-		tempFile.delete();
-		
-		
-		//To use XGBoost4J, we convert csv format features obtain above to libsvm format.		
-		String[] featureList = features.split(",");
-		String libsvmFeature="";
-		for(int i=0; i <featureList.length;i++ ){
-			if(!"0".equals(featureList[i])) {
-				libsvmFeature=libsvmFeature+(i+1)+":"+featureList[i]+" ";
+			tempFile.delete();
+
+			// To use XGBoost4J, we convert csv format features obtain above to libsvm
+			// format.
+			String[] featureList = features.split(",");
+			String libsvmFeature = "";
+			for (int i = 0; i < featureList.length; i++) {
+				if (!"0".equals(featureList[i])) {
+					libsvmFeature = libsvmFeature + (i + 1) + ":" + featureList[i] + " ";
+				}
 			}
-		}		
-		return libsvmFeature;
-		
-		} catch( Exception e ){
+			return libsvmFeature;
+
+		} catch (Exception e) {
 			return "";
 		}
 	}
--- src/test/java/fr/inria/coming/spoon/features/RepairnatorFeatureTest.java
+++ src/test/java/fr/inria/coming/spoon/features/RepairnatorFeatureTest.java
@@ -1,23 +1,41 @@
 package fr.inria.coming.spoon.features;
 
 import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+
 import org.junit.Test;
+
 import fr.inria.coming.codefeatures.RepairnatorFeatures;
 import fr.inria.coming.codefeatures.RepairnatorFeatures.ODSLabel;
+import fr.inria.coming.main.ComingProperties;
 
 public class RepairnatorFeatureTest {
- 
-	 	@Test
-		public void correctPatch() throws Exception  {
-		 File pairFolder = new File("src/main/resources/Defects4J_all_pairs/Lang_36");
-		 ODSLabel label = new RepairnatorFeatures().getLabel(pairFolder);
-		 assertEquals(label,ODSLabel.CORRECT);
-	 }
-	 	@Test
-	 	public void unknownPatch() throws Exception  {
-			 File pairFolder = new File("src/main/resources/Defects4J_all_pairs/Math_42");
-			 ODSLabel label = new RepairnatorFeatures().getLabel(pairFolder);
-			 assertEquals(label,ODSLabel.UNKNOWN);
-		 }
+
+	@Test
+	public void correctPatch() throws Exception {
+		File pairFolder = new File("src/main/resources/Defects4J_all_pairs/Lang_36");
+		ODSLabel label = new RepairnatorFeatures().getLabel(pairFolder);
+		assertEquals(label, ODSLabel.CORRECT);
+	}
+
+	@Test
+	public void unknownPatch() throws Exception {
+		File pairFolder = new File("src/main/resources/Defects4J_all_pairs/Math_42");
+		ODSLabel label = new RepairnatorFeatures().getLabel(pairFolder);
+		assertEquals(label, ODSLabel.UNKNOWN);
+	}
+
+	@Test
+	public void correctPatchAbsoluteLocationModel() throws Exception {
+		File locationToModel = new File("./src/main/resources/ODSTraining/ODSmodel.bin");
+		String locationString = locationToModel.getAbsolutePath();
+		ComingProperties.setProperty(RepairnatorFeatures.ODS_MODEL, locationString);
+
+		assertEquals(ComingProperties.getProperty(RepairnatorFeatures.ODS_MODEL), locationString);
+
+		File pairFolder = new File("src/main/resources/Defects4J_all_pairs/Lang_36");
+		ODSLabel label = new RepairnatorFeatures().getLabel(pairFolder);
+		assertEquals(label, ODSLabel.CORRECT);
+	}
 }
