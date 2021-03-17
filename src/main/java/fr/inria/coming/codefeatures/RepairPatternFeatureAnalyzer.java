99cb46a29fed5b96771bc6f9bd720a19aad3a0a9
Sophie H Ye <he_ye_90s@hotmail.com>
2021-03-17 08:50:21 +0100

    added new ODS features (#247)
    * fix ODS no feature generation bug
    
    * updated the test case to present the result of after fixing ODS bug
    
    * removed the comments
    
    * added new repair pattern features
    
    * rm the system out printing log
    
    * added a test for new repair pattern features

---
+++ src/main/java/fr/inria/coming/codefeatures/RepairPatternFeatureAnalyzer.java
@@ -0,0 +1,326 @@
+package fr.inria.coming.codefeatures;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import com.github.difflib.DiffUtils;
+import com.github.difflib.patch.AbstractDelta;
+import com.github.difflib.patch.Chunk;
+import com.github.difflib.patch.Patch;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import gumtree.spoon.diff.Diff;
+import gumtree.spoon.diff.operations.Operation;
+
+public class RepairPatternFeatureAnalyzer {
+	 static int dupArgsInvocation = 0; //duplicate variables in method invocation
+	 static int updIfFalse = 0;  //if false to replace if condition
+	 static int insertIfFalse = 0;  //
+
+	 static int ifTrue = 0;  //if true to replace if condition
+	 static int condLogicReduce = 0;  //if false to replace if condition
+	 static int insertBooleanLiteral = 0; //a varaible is replaced by true or false
+	 static int insertNewConstLiteral = 0; // const, false, true
+	 static int UpdateLiteral = 0 ;
+	 static int removeNullinCond = 0 ; //remove null in if
+	 static int	patchedFileNo=0;
+	 static int	rmLineNo=0;
+	 static int	addLineNo=0;
+	 static int	 addThis=0;
+
+
+
+	public static JsonObject analyze(String folderPath, Diff diff, String targetFile) {
+		
+		dupArgsInvocation = 0;
+		updIfFalse = 0; 
+		insertIfFalse = 0;
+		ifTrue = 0;  
+		condLogicReduce = 0;  
+		insertBooleanLiteral = 0; 
+		insertNewConstLiteral = 0;
+		UpdateLiteral = 0;
+		removeNullinCond = 0;
+		patchedFileNo=0;
+		rmLineNo=0;
+		addLineNo=0;
+		addThis=0;
+		
+		Map<String, File> filePaths = new P4JFeatureAnalyzer().processFilesPair(new File(folderPath),targetFile);
+		File src = filePaths.get("src");
+		File target = filePaths.get("target");
+		
+		
+      patchAnalysis(folderPath);
+	  // if(false) in one line conditional replacement
+	  ifFalseAnalyzer(folderPath, diff, target);	
+	  ifTrueAnalyzer(folderPath, diff, target);	
+	  //dupArgsInvocation if duplicated arguments in one method invocation 
+	  dupArgsInvocation(folderPath, diff, target);
+	  condLogicReduceAnalyzer(folderPath, diff, src,target);
+	  insertLiteralAnalyzer(folderPath, diff,src, target);
+	  rmNullAnalyzer(folderPath, diff, src,target);
+	  addThisAnalyzer(folderPath, diff, src,target);
+
+
+      JsonObject jsonObject = new JsonObject();
+      jsonObject.addProperty("updIfFalse", updIfFalse);
+      jsonObject.addProperty("insertIfFalse", insertIfFalse);
+      jsonObject.addProperty("ifTrue", ifTrue);
+      jsonObject.addProperty("dupArgsInvocation", dupArgsInvocation);
+      jsonObject.addProperty("condLogicReduce", condLogicReduce);
+      jsonObject.addProperty("insertNewConstLiteral", insertNewConstLiteral);
+      jsonObject.addProperty("insertBooleanLiteral", insertBooleanLiteral);
+      jsonObject.addProperty("UpdateLiteral", UpdateLiteral);
+      jsonObject.addProperty("removeNullinCond", removeNullinCond);
+      jsonObject.addProperty("patchedFileNo", patchedFileNo);
+      jsonObject.addProperty("rmLineNo", rmLineNo);
+      jsonObject.addProperty("addLineNo", addLineNo);
+      jsonObject.addProperty("addThis", addThis);
+
+	  return jsonObject;
+
+	}
+
+	private static void addThisAnalyzer(String folderPath, Diff diff, File src, File target) {
+		List<Operation> operations = diff.getRootOperations();
+		for (Operation opt : operations) {
+			if (opt.toString().contains("Insert") ) {
+				String newvar = opt.toString().split(":")[1].split("\n\t")[1];
+				if(newvar.contains("this")) {
+					addThis = 1;
+				}
+			}
+		}
+		
+	}
+
+	private static void patchAnalysis(String folderPath) {
+		Map<String, File> pathmap = new HashMap();
+		File pairFolder = new File(folderPath);
+
+		for (File fileModif : pairFolder.listFiles()) {
+
+			if (!".DS_Store".equals(fileModif.getName())) {
+				patchedFileNo ++;
+				
+				String pathname = fileModif.getAbsolutePath() + File.separator + pairFolder.getName() + "_"
+						+ fileModif.getName();
+				File src = new File(pathname + "_s.java");
+				File target = new File(pathname + "_t.java");
+				try {
+				List<String> original = IOUtils.readLines(new FileInputStream(src), "UTF-8");
+		        List<String> revised = IOUtils.readLines(new FileInputStream(target), "UTF-8");
+		        Patch<String> diff = DiffUtils.diff(original, revised);
+		        List<AbstractDelta<String>> deltas = diff.getDeltas();
+		        deltas.forEach(delta -> {
+		            switch (delta.getType()) {
+		                case INSERT:
+		                    Chunk<String> insert = delta.getTarget();
+		                    addLineNo += insert.size();
+		                    break;
+		                case CHANGE:
+		                    Chunk<String> source = delta.getSource();
+		                    Chunk<String> target1 = delta.getTarget();
+		                    rmLineNo += source.size();
+		                    addLineNo += target1.size();
+		                    break;
+		                case DELETE:
+		                    Chunk<String> delete = delta.getSource();
+		                    rmLineNo += delete.size();
+		                    break;
+		            }
+
+		        });
+		        
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+				
+				
+			}
+				
+		}
+	}
+		
+
+
+	private static void rmNullAnalyzer(String folderPath, Diff diff, File source, File target) {
+		List<Operation> operations = diff.getAllOperations();
+		for (Operation opt : operations) {
+			if (opt.toString().contains("Delete BinaryOperator") ) {
+				String deleted = opt.toString().split(":")[1].split("\n\t")[1];
+				String specificNo = opt.toString().split(":")[1].split("\n\t")[0];
+				String sourceline = getSpecificLine(source.getPath(), Integer.parseInt(specificNo) - 1);		
+				String targetline = getSpecificLine(target.getPath(), Integer.parseInt(specificNo) - 1);
+				if (deleted.contains("null") && sourceline.contains("if") && targetline.contains("if")) {
+					removeNullinCond = 1;
+				}
+			}
+		}
+		
+		
+	}
+
+	private static void insertLiteralAnalyzer(String folderPath, Diff diff, File source, File target) {
+		// TODO Auto-generated method stub
+		List<Operation> operations = diff.getAllOperations();
+		for (Operation opt : operations) {
+			if (opt.toString().contains("Insert Literal") ) {
+				String specificNo = opt.toString().split(":")[1].split("\n\t")[0];
+				String newLiteral = opt.toString().split(":")[1].split("\n\t")[1];
+				if (!newLiteral.contains("true") && !newLiteral.contains("false")) {
+					newLiteral = newLiteral.trim().replace("\n", "");
+					
+					try {
+				        Double.parseDouble(newLiteral);
+				    } catch (NumberFormatException nfe) {
+				        return;
+				    }
+					
+					for(int i = -2; i<3;i++) {
+						String sourceline = getSpecificLine(source.getPath(), Integer.parseInt(specificNo) +i);	
+						if (sourceline.contains(newLiteral)) {
+							return;
+						}
+					}
+					insertNewConstLiteral = 1;
+					
+					
+					break;
+				} else {
+					String targetline = getSpecificLine(target.getPath(), Integer.parseInt(specificNo) - 1);
+					targetline=targetline.replace(" ", "");
+					if (targetline.contains("==true") && targetline.contains("==false")) {
+						insertBooleanLiteral = 1;
+					}
+				}
+			}	else if 	(opt.toString().contains("Update Literal") ) {
+				UpdateLiteral = 1;
+			}
+		}
+	}
+
+
+	private static void condLogicReduceAnalyzer(String folderPath, Diff diff, File source,  File target) {
+		List<Operation> operations = diff.getRootOperations();
+		for (Operation opt : operations) {
+			if (opt.toString().contains("Delete BinaryOperator") ) {
+				String specificNo = opt.toString().split(":")[1].split("\n\t")[0];
+				String deletedItem = opt.toString().split("\n\t")[1];
+				String sourceline = getSpecificLine(source.getPath(), Integer.parseInt(specificNo) - 1);		
+				String targetline = getSpecificLine(target.getPath(), Integer.parseInt(specificNo) - 1);
+				if (sourceline.contains("if") && (deletedItem.contains("&") || deletedItem.contains("||"))) {
+					if(targetline.contains("if")) {
+						condLogicReduce = 1;
+					}
+				}
+
+			}
+		}
+	}
+
+
+
+	private static void dupArgsInvocation(String folderPath, Diff diff, File target) {
+		List<Operation> operations = diff.getRootOperations();
+		for (Operation opt : operations) {
+			if (opt.toString().contains("Update VariableRead") || opt.toString().contains("Update BinaryOperator") ) {
+				String specificNo = opt.toString().split(":")[1].split("\n\t")[0];
+				String oldVar = opt.toString().split(":")[1].split("\n\t")[1].split("to")[0];
+				String newVar = opt.toString().split(":")[1].split("\n\t")[1].split("to")[1];
+				newVar = newVar.replace("\n", "").trim();
+				String targetline = getSpecificLine(target.getPath(), Integer.parseInt(specificNo) - 1);			    
+				targetline = targetline.trim();
+				if(!targetline.startsWith("/")) {
+				int count = (targetline.split(newVar,-1).length ) - 1;
+				if (count >= 2) {
+					if (!targetline.contains("?") && !targetline.contains(":")) {
+						dupArgsInvocation = 1;
+					}
+				}				
+			}		
+			}
+		}		
+	}
+
+
+	private static void ifFalseAnalyzer(String folderPath, Diff diff, File target) {
+		List<Operation> operations = diff.getRootOperations();
+		try {
+		for (Operation opt : operations) {
+			if (opt.getAction().getName().contains("INS")) {
+				String specificNo = opt.toString().split(":")[1].split("\n\t")[0];
+				String targetline = getSpecificLine(target.getPath(), Integer.parseInt(specificNo) - 1);
+				targetline = targetline.replace(" ", "");
+				if (opt.toString().contains("Insert If")) {
+					if ((targetline).contains("if(false)")) {
+						insertIfFalse = 1;
+					}
+					if(diff.toString().contains("if (false)")) {
+						insertIfFalse = 1;
+					}
+				}else {				
+				if ((targetline).contains("if(false)")) {
+					updIfFalse = 1;
+				}
+				if(diff.toString().contains("if (false)")) {
+					updIfFalse = 1;
+				}
+			}		
+			}
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+	private static void ifTrueAnalyzer(String folderPath, Diff diff, File target) {
+		List<Operation> operations = diff.getRootOperations();
+		try {
+		for (Operation opt : operations) {
+			if (opt.getAction().getName().contains("INS")) {
+				String specificNo = opt.toString().split(":")[1].split("\n\t")[0];
+				String targetline = getSpecificLine(target.getPath(), Integer.parseInt(specificNo) - 1);
+				targetline = targetline.replace(" ", "");
+				if ((targetline).contains("if(true)")) {
+					ifTrue = 1;
+				}
+				if(diff.toString().contains("if (true)")) {
+					ifTrue = 1;
+				}
+			}						
+		}
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	}
+		
+	
+	
+
+	public static String getSpecificLine(String filePath, int lineNo) {
+		String targetline = "";
+		try {
+			targetline = Files.readAllLines(Paths.get(filePath)).get(lineNo);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return targetline;
+		}
+		return targetline;
+	}
+}
